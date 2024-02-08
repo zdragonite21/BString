defb635a54d62355315b31f5895c185ed18f588a
zdragonite21 <72771809+zdragonite21@users.noreply.github.com>
2024-02-07 22:10:00 -0800

    Update stringBase.py

--- stringBase.py
+++ stringBase.py
@@ -5,13 +5,13 @@ import numpy as np
 start = bpy.data.objects["start"].location
 end = bpy.data.objects["end"].location
 dist = np.linalg.norm(end - start)
-depth = dist /500
+depth = dist / 500
 
 # string 1
 stringPts1 = 3
-curve1 = bpy.data.curves.new("String", "CURVE") 
+curve1 = bpy.data.curves.new("String", "CURVE")
 nurbs1 = curve1.splines.new("NURBS")
-obj1 = bpy.data.objects.new("String.01.01", curve1) 
+obj1 = bpy.data.objects.new("String.01.01", curve1)
 bpy.context.collection.objects.link(obj1)
 
 curve1.dimensions = "3D"
@@ -21,22 +21,22 @@ curve1.bevel_depth = depth
 nurbs1.points.add(stringPts1 - 1)
 
 nurbs1.points[0].co = [0, 0, 0, 1]
-nurbs1.points[1].co = [dist/4, 0, 0, 1]
-nurbs1.points[2].co = [dist/2, 0, 0, 1]
+nurbs1.points[1].co = [dist / 4, 0, 0, 1]
+nurbs1.points[2].co = [dist / 2, 0, 0, 1]
 
 nurbs1.use_endpoint_u = True
 nurbs1.order_u = stringPts1
 
 # obj1 transformations
 obj1.location = (start + end) / 2
-objDir1 = (end - start)
+objDir1 = end - start
 obj1.rotation_euler = objDir1.to_track_quat("X", "Z").to_euler()
 
 # string 2
 stringPts2 = 2
-curve2 = bpy.data.curves.new("String", "CURVE") 
+curve2 = bpy.data.curves.new("String", "CURVE")
 nurbs2 = curve2.splines.new("NURBS")
-obj2 = bpy.data.objects.new("String.01.02", curve2) 
+obj2 = bpy.data.objects.new("String.01.02", curve2)
 bpy.context.collection.objects.link(obj2)
 
 curve2.dimensions = "3D"
@@ -53,5 +53,5 @@ nurbs2.order_u = stringPts2
 
 # obj2 transformations
 obj2.location = (start + end) / 2
-objDir2 = (end - start)
+objDir2 = end - start
 obj2.rotation_euler = objDir2.to_track_quat("X", "Z").to_euler()
